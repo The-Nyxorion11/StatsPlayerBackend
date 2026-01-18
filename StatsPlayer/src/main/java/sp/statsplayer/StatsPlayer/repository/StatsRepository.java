3a1a5d7bf3cbea31a0d956b29c28dd852b118737
The-Nyxorion11 <siiraxmc@gmail.com>
2026-01-17 20:43:25 -0500

    Revert "1.0.0"
    This reverts commit 257d797e6c9a05b5a6ea098c31d276112c58cd42.

--- StatsPlayer/src/main/java/sp/statsplayer/StatsPlayer/repository/StatsRepository.java
+++
@@ -1,7 +0,0 @@
-package sp.statsplayer.StatsPlayer.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import sp.statsplayer.StatsPlayer.model.Stats;
-
-public interface StatsRepository extends JpaRepository<Stats, String> {
-}
